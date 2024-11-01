adc5090d70c7ac46f6ed0e98fd86c0b552dd822d
Axel Rautiainen <axerau-0@student.ltu.se>
2024-11-01 19:37:29 +0100

    Finished adding questions, input verification, point tracking

--- AxelRautiainen.js
+++ AxelRautiainen.js
@@ -7,8 +7,8 @@ const emailError = document.getElementById("emailError");
 const q1Info = document.getElementById("q1Info");
 const q2Info = document.getElementById("q2Info");
 const q3Info = document.getElementById("q3Info");
-
-const submitError = document.getElementById("submitError");
+const q4Info = document.getElementById("q4Info");
+const q5Info = document.getElementById("q5Info");
 
 form.addEventListener("submit", function(event) {
 
@@ -19,43 +19,53 @@ form.addEventListener("submit", function(event) {
     let isValid = true;
     let score = 0;
 
-    // Validate first name
+    // Validate first name (check if not empty + only contains letters)
     const firstName = document.getElementById("fname");
-    if (!firstName.value.match(/^[A-Za-z]+$/)) {
+    if (firstName.value.trim() === '') {
         isValid = false;
-        fnameError.textContent = "First name should only contain letters"
+        fnameError.textContent = "First name is required";
+    } else if (!firstName.value.match(/^[A-Za-z]+$/)) {
+        isValid = false;
+        fnameError.textContent = "First name should only contain letters";
     }
 
-    // Validate last name
+    // Validate last name (check if not empty + only contains letters)
     const lastName = document.getElementById("lname");
-    if (!lastName.value.match(/^[A-Za-z]+$/)) {
+    if (lastName.value.trim() === '') {
         isValid = false;
-        lnameError.textContent = "Last name should only contain letters"
+        lnameError.textContent = "Last name is required";
+    } else if (!lastName.value.match(/^[A-Za-z]+$/)) {
+        isValid = false;
+        lnameError.textContent = "Last name should only contain letters";
     }
 
-    // Validate email
+    // Validate email (check if not empty + contains @)
     const email = document.getElementById("email");
-    if (!email.value.includes('@')) {
+    if (email.value.trim() === '') {
         isValid = false;
-        emailError.textContent = "Invalid email, address should contain @"
+        emailError.textContent = "Email is required";
+    } else if (!email.value.includes('@')) {
+        isValid = false;
+        emailError.textContent = "Invalid email, address should contain @";
     }
 
     // Validate question 1
     const q1 = document.querySelectorAll('input[name="colors"]:checked');
+    let q1Incorrect = false;
     if (q1.length === 0) {
         isValid = false;
-        q1Info.textContent = "Please answer this question"
+        q1Info.textContent = "Please answer this question";
     } else if (q1[0].id === "Blue") {
         score++;
     } else {
-        q1Info.textContent = "0/1 correct"
+        q1Incorrect = true;
     }
 
     // Validate question 2
     const q2 = document.getElementById("q2");
     if (q2.value.trim() === '') {
         isValid = false;
-        q2Info.textContent = "Please answer this question"
+        q2Info.textContent = "Please answer this question";
     }
 
     // Validate question 3
@@ -63,6 +73,9 @@ form.addEventListener("submit", function(event) {
     const q3Answers = [];
     const q3Right = ['C++', 'Python', 'JavaScript'];
     const q3Wrong = ['Linux', 'Windows'];
+    let awarded = 0;
+    let numOfRightAnswers = 0;
+    let numOfWrongAnswers = 0;
     q3.forEach(option => {
         if (option.checked) {
             q3Answers.push(option.value);
@@ -70,25 +83,72 @@ form.addEventListener("submit", function(event) {
     });
     for (let i = 0; i < q3Right.length; i++) { // Loop through correct answers and award a point for each
         if (q3Answers.includes(q3Right[i])) {
-            score++;
+            awarded++;
+            numOfRightAnswers++;
         }
     }
-    for (let i = 0; i < q3Wrong.length; i++) { // Subtract a point for each incorrect answer
+    for (let i = 0; i < q3Wrong.length; i++) { // But if an incorrect answer is chosen, award 0 points for this question
         if (q3Answers.includes(q3Wrong[i])) {
-            score--;
+            awarded = 0;
+            numOfWrongAnswers++;
         }
+    }
+    score = score + awarded;
+
+    // Validate question 4
+    const q4 = document.querySelectorAll('input[name="capitals"]:checked');
+    let q4Incorrect = false;
+    if (q4.length === 0) {
+        isValid = false;
+        q4Info.textContent = "Please answer this question";
+    } else if (q4[0].id === "brussels") {
+        score++;
+    } else {
+        q4Incorrect = true;
+    }
+
+    // Validate question 5
+    const q5 = document.getElementById("q5");
+    let q5Incorrect = false;
+    if (q5.value.trim() === '') {
+        isValid = false;
+        q5Info.textContent = "Please answer this question";
+    } else if (!q5.value.trim().match(/^\d+$/)) {
+        isValid = false;
+        q5Info.textContent = "Please only enter a number";
+    } else if (q5.value.trim() === "110") {
+        score++;
+    } else {
+        q5Incorrect = true;
     }
 
     // Check if all criteria are met for submission
     if (isValid) {
         alert(`Your answers were submitted successfully. Thank you for participating.\nYou scored ${score} points out of 6.`)
+        if (q1Incorrect) {
+            q1Info.textContent = "Incorrect";
+        } else {
+            q1Info.textContent = "Correct!";
+        }
+
+        q3Info.textContent = `${numOfRightAnswers}/3 correct, ${numOfWrongAnswers} incorrect`;
+
+        if (q4Incorrect) {
+            q4Info.textContent = "Incorrect";
+        } else {
+            q4Info.textContent = "Correct!";
+        }
+
+        if (q5Incorrect) {
+            q5Info.textContent = "Incorrect";
+        } else {
+            q5Info.textContent = "Correct!";
+        }
+
     } else {
         alert("Missing answers or invalid inputs")
-        //submitError.textContent = "Missing answers or invalid inputs"
     }
 
-    console.log(q3Answers)
-    console.log(score)
 });
 
 function clearErrors() {
